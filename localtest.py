c8d2018c29276b65ee4e434dd50ae2ae9deccdbf
Olivier Lamothe <olamothe@coveo.com>
2021-05-18 09:24:58 -0400

    feat(docbuilder): add doc builder class (#2)
    https://coveord.atlassian.net/browse/CDX-333

--- localtest.py
+++ localtest.py
@@ -1,4 +1,14 @@
-from src.push_api_clientpy import DocumentBuilder, Document
+from src.push_api_clientpy import DocumentBuilder
+import datetime
 
-docBuilder = DocumentBuilder('id', 'title')
-print(docBuilder)
+docBuilder = DocumentBuilder('id', 'title')\
+    .withDate(datetime.datetime.now())\
+    .withModifiedDate("2000/01/01")\
+    .withClickableUri('https://clicky.uri')\
+    .withFileExtension('.html')\
+    .withParentId('parent_id')\
+    .withMetadata({"foo": "bar", "bar": ["buzz"]})\
+    .withMetadataValue("hello", [1, 2, 3])
+
+
+print(docBuilder.marshal())
